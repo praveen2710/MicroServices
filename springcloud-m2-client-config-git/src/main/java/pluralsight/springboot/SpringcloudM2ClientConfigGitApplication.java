46a45dc2e7a12f62594b9257d461da5973bf5f90
Praveen Banthia <pbanthia@masonlive.gmu.edu>
2017-05-18 02:48:29 -0500

    adding client service

---
+++ springcloud-m2-client-config-git/src/main/java/pluralsight/springboot/SpringcloudM2ClientConfigGitApplication.java
@@ -0,0 +1,12 @@
+package pluralsight.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudM2ClientConfigGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudM2ClientConfigGitApplication.class, args);
+	}
+}
