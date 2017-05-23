d5f1b466916b09b758c20520928f77f5102dd6ca
Praveen Banthia <pbanthia@masonlive.gmu.edu>
2017-05-23 00:42:40 -0500

    adding custom spans

--- pluralsight-springcloud-m5-dataservice2/src/main/java/pluralsight/demo/PluralsightSpringcloudM5Dataservice2Application.java
+++ pluralsight-springcloud-m5-dataservice2/src/main/java/pluralsight/demo/PluralsightSpringcloudM5Dataservice2Application.java
@@ -3,8 +3,11 @@ package pluralsight.demo;
 import java.util.Hashtable;
 import java.util.Random;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.Span;
+import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,16 +22,36 @@ public class PluralsightSpringcloudM5Dataservice2Application {
 		SpringApplication.run(PluralsightSpringcloudM5Dataservice2Application.class, args);
 	}
 	
+	@Autowired
+	private Tracer tracer;
+	
 	@RequestMapping(value="/customer/{cid}/vehicledetails", method=RequestMethod.GET)
-	public @ResponseBody String getCustomerVehicleDetails(@PathVariable Integer cid) {
+	public @ResponseBody String getCustomerVehicleDetails(@PathVariable Integer cid) throws InterruptedException {
 		
-		Hashtable<Integer, String> vehicles = new Hashtable<Integer, String>();
-		vehicles.put(100, "Lincoln Continental; Plate SNUG30");
-		vehicles.put(101, "Chevrolet Camaro; Plate R7TYR43");
-		vehicles.put(102, "Volkswagen Beetle; Plate 6CVI3E2");
+		String result ;
 		
-		String result = vehicles.get(cid);
+		Span s = this.tracer.createSpan("lookup Vehicle");
 		
+		try{
+			
+			this.tracer.addTag("customerId", cid.toString());
+			
+			s.logEvent("Database query started");
+			
+			Thread.sleep(500);
+			
+			Hashtable<Integer, String> vehicles = new Hashtable<Integer, String>();
+			vehicles.put(100, "Lincoln Continental; Plate SNUG30");
+			vehicles.put(101, "Chevrolet Camaro; Plate R7TYR43");
+			vehicles.put(102, "Volkswagen Beetle; Plate 6CVI3E2");
+			
+			result = vehicles.get(cid);
+			
+			s.logEvent("Database query finished");
+			
+		}finally {
+			this.tracer.close(s);
+		}
 		return result;
 	}
 }
